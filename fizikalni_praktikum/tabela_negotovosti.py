4a0857351f5acd7572dadac3e461e0527307a883
gregpr07 <gregor.ninjaboy@gmail.com>
2019-12-22 13:30:32 +0100

    rezultat

--- fizikalni_praktikum/tabela_negotovosti.py
+++ fizikalni_praktikum/tabela_negotovosti.py
@@ -22,6 +22,9 @@ kt v Projekt Tomo se spreminja samo stvari k so med ##   ##
 # to je samo vizualno da se vid kaj se za rezultat dubi
 racunamo = 'k'
 
+# stevilo racunskih mest pri koncnem rezultatu (output)
+natancnost_mest = 3
+
 # vazn samo za koncn output, za velikost texta
 text_size = 20
 
@@ -31,7 +34,7 @@ ro, d1, d2 = symbols(r'\rho d_1 d_2')
 # za vsako neznanko nrdimo tuple, (x, []) prvi el v listu je velikost neznanke, drugi el je napaka
 data = [
     (ro, [13000, 0]),
-    (d1, [0.006, 0.1*10**-6]),
+    (d1, [0.006, 0.00001]),
     (d2, [0.0128, 0.1*10**-6])
 ]
 
@@ -43,9 +46,13 @@ function = ro*(1/(pi*d2**2)**2-1/(pi*d1**2)**2)/2
 
 # ne spreminjat, mislm lah ampak se lah kej breaka
 
+def format_text(num):
+    return str(f'%.{natancnost_mest}E' % num).replace('+','')
+
 values = [(x[0], x[1][0]) for x in data]
 
 results = []
+formated_results = []
 
 for el in data:
     deriv = Derivative(function, el[0])
@@ -53,10 +60,13 @@ for el in data:
     deltaa = el[1][1]
     error = (float(deriv.doit().subs(values) * deltaa))
     latfunc = '$'+latex(res)+'$'
-    results.append((latfunc, deltaa, '%.4E' % Decimal(error)))
+    results.append((latfunc, deltaa, error))
+    formated_results.append((latfunc, deltaa, format_text(error)))
+
+
+vrednost_f = float(function.doit().subs(values))
 
 final_error = sqrt(sum([abs(float(x[2])**2) for x in results]))
-print(final_error)
 
 if not text_size:
     text_size = 15
@@ -74,7 +84,7 @@ plt.subplots_adjust(left=0.05, bottom=0, right=0.97, top=0.94)
 
 plt.axis('off')
 
-table = plt.table(cellText=results, loc='center left',
+table = plt.table(cellText=formated_results, loc='center left',
                   rowLabels=rows, colLabels=columns)
 
 plt.text(0.35, 0.85, '$'+racunamo+'=' +
@@ -83,8 +93,14 @@ table.set_fontsize(text_size)
 table.scale(1, 4)
 table.auto_set_font_size(False)
 
-plt.text(0.35, 0.1, r'$\Delta '+racunamo+' = ' +
-         '%.4E' % final_error+'$', fontsize=text_size+4)
+
+latex_vrednost = format_text(vrednost_f)
+latex_error = format_text(final_error)
+
+print(latex_vrednost,'+-',latex_error)
+
+plt.text(0.02, 0.1, r'$'+racunamo+' = ' + latex_vrednost + ' \pm ' +
+         latex_error +'$', fontsize=text_size+4)
 
 
 plt.show()
